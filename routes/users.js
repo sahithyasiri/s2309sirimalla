eca324167aead347d0af5489255734656aff5a06
sahithyasiri <sahithyasirimalla27@gmail.com>
2023-03-29 19:37:22 -0500

    Removed uncommented code from users.js

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 // var sum=0;
 // var more=0;
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   more=more+1;
-//   sum=sum=more
-//   res.send('Sum is: '+sum);
-// });
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // more=more+1;
+  // sum=sum=more
+  // res.send('Sum is: '+sum);
+  res.send('respond with a resource');
+});
 
-// module.exports = router;
+module.exports = router;
