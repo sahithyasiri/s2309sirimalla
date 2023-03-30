70e772a13ec4a019f927f133520e54f1cc93bbb8
sahithyasiri <sahithyasirimalla27@gmail.com>
2023-03-29 20:54:37 -0500

    added changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var indexRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mydata', indexRouter);
+app.use('/mydata', mydataRouter);
 
 
 // catch 404 and forward to error handler
