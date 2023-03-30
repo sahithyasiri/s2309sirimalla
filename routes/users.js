db017755b3857525d1a7cf37070f6a2aff229623
sahithyasiri <sahithyasirimalla27@gmail.com>
2023-03-29 19:23:08 -0500

    Added commented code

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var sum=0;
-var more=0;
+// var express = require('express');
+// var router = express.Router();
+// var sum=0;
+// var more=0;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  more=more+1;
-  sum=sum=more
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   more=more+1;
+//   sum=sum=more
+//   res.send('Sum is: '+sum);
+// });
 
-module.exports = router;
+// module.exports = router;
