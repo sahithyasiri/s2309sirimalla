3a703589a375014eafb521b27e67f519ff3175b8
sahithyasiri <sahithyasirimalla27@gmail.com>
2023-03-29 19:42:21 -0500

    Added uncommented code file from index.js

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-// var sum=0;
-// var more=0;
+var sum=0;
+var more=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // more=more+1;
-  // sum=sum=more
-  // res.send('Sum is: '+sum);
+  more=more+1;
+  sum=sum=more
+  res.send('Sum is: '+sum);
   res.send('respond with a resource');
 });
 
